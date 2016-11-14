f76a42132c4b2e5fea19c5e9ef34ec752b2169e9
Martinák Zalán <mazalan01@gmail.com>
2016-11-14 12:39:21 +0100

    Minden 3. fizz

--- Fizz.cpp
+++ Fizz.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main(){
-    for(int i=0;i<=100;i++)cout<<i<<endl;
+    for(int i=0;i<=100;i++){
+        if(i%3==0){
+            cout<<"Fizz"<<endl;
+        }
+        else{
+            cout<<i<<endl;
+        }
+    }
     return 0;
 }
